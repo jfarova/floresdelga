d741bfafefaf901765a8c46d4513e35eb778b624
chenaoh <jfarova112333@gmail.com>
2022-11-20 22:20:01 -0500

    por fin

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int l1,l2,l3;
+        Console.WriteLine("ingresa lado 1");
+       l1=int.Parse(Console.ReadLine());
+       Console.WriteLine("ingresa lado 2");
+       l2=int.Parse(Console.ReadLine());
+       Console.WriteLine("ingresa lado 3");
+       l3=int.Parse(Console.ReadLine());
+       if(l1 == l2  && l2 == l3){
+        Console.WriteLine("los dados correspuediente a un triangulo equilatero ");}
+        else if(l1==l2 || l1==l3 || l2 ==l3){
+         Console.WriteLine("los lados correspoden a un triangulo isosceles");
+        }
+     
+        else
+        {
+            Console.WriteLine("los lados correspondiente a un triangulo escaleno");
+        }
+        
+        }
+       }
+    
+   
+
+
+
